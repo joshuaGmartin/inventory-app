812dddc51e48723876cd48099929d30aa22ecbb7
joshuaGmartin <156712569+joshuaGmartin@users.noreply.github.com>
2026-03-17 10:39:54 -0500

    Init server and routes setup

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+const path = require("node:path");
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+const assetPath = path.join(__dirname, "public");
+app.use(express.static(assetPath));
+app.use(express.urlencoded({ extended: true }));
+
+const indexRouter = require("./routes/indexRouter");
+
+app.use("/", indexRouter);
+
+// 404
+app.use((req, res) => {
+  res.status(404).send("Page does not exist");
+});
+
+// errors
+app.use((err, req, res, next) => {
+  console.log(err.sta);
+  res.status(err.statusCode || 500).send(err.message);
+});
+
+const PORT = 3000;
+app.listen(PORT, (error) => {
+  if (error) throw error;
+
+  console.log("Listening on port:", PORT);
+});
---
+++ controllers/indexController.js
@@ -0,0 +1,9 @@
+// require db/queries, body
+
+const validator = [];
+
+async function getAllFilms(req, res) {
+  res.send("lists all films - wip");
+}
+
+module.exports = { getAllFilms };
