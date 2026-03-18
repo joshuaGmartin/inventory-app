d3e7255afbb7135df32f5a1723335bdb25822768
joshuaGmartin <156712569+joshuaGmartin@users.noreply.github.com>
2026-03-18 11:03:28 -0500

    Fix bugs in DB setup. Creat allFilms query

--- db/queries.js
+++ db/queries.js
@@ -1,7 +1,9 @@
 const pool = require("./pool");
 
 const getAllFilms = async (req, res) => {
-  const { rows } = await pool.query("SELECT * FROM directors");
+  const { rows } = await pool.query(
+    "SELECT title, release_year, dir_name, genre_name FROM films JOIN directors ON director_id = directors.id JOIN genres ON genre_id = genres.id",
+  );
   console.log(rows);
 };
 
